3c4fd03c7edbafd5b13be60a8f541f68c9f5b56d
Mauricio Jiménez <maupzx@gmail.com>
2021-10-29 23:10:24 -0600

    Agregado código Examen 1

--- HolaMundo/src/main/java/com/java/holamundo/Variables.java
+++ HolaMundo/src/main/java/com/java/holamundo/Variables.java
@@ -22,5 +22,29 @@ public class Variables {
         String titulo = "Estudiante";
         String union = usuario + titulo;
         System.out.println("Union = " + union);
+        
+        int a= 8; int b= 3; int c=-5;
+        int result12 = 2 * (a / 5 + (4 - b *3)) % (a + c - 2);
+        System.out.println("Result 12 = " + result12);
+        int result = a - b - c * 2;
+        System.out.println("Result 13 = " + result);
+        int result14 = (3 * a - 2 * b) % (2 * a - c);
+        System.out.println("Result 14 = " + result14);
+        int result15 =  a - b - c * 2;
+        System.out.println("Result 15 = " + result15);
+        
+        String color;
+        //System.out.println(color);
+        //color = “Rojo”;
+
+        String nombre = "Juan";
+        System.out.println(nombre);
+        nombre = "Carlos";
+        System.out.println(nombre);
+        
+        // a = "A";
+        //System.out.println(a);
+        // a = "B";
+        // System.out.println(b);
     }
 }
--- LeerConsola/src/leerconsola/LeerConsola.java
+++ LeerConsola/src/leerconsola/LeerConsola.java
@@ -19,11 +19,49 @@ public class LeerConsola {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner consola = new Scanner(System.in);
-        System.out.println("Proporciona el título: ");
+        /*System.out.println("Proporciona el título: ");
         String titulo = consola.nextLine();
         System.out.println("Proporciona el autor: ");
         String autor = consola.nextLine();
         System.out.println(titulo + " fue escrito por " + autor);
+        */
+        // Preg Examen 8
+        /*int num1, num2;
+        do {
+            System.out.println("Proporciona el Número 1: ");
+            num1 = Integer.parseInt(consola.nextLine());
+            System.out.println("Proporciona el Número 2: ");
+            num2 = Integer.parseInt(consola.nextLine());
+        } while (num1 != num2);
+        */
+        // Preg 16 Examen
+        /*int i=1, numero=0, suma=0, positivos=0, negativos=0;
+        do {
+            System.out.println("Proporciona el Número " + i);
+            numero = Integer.parseInt(consola.nextLine());
+            suma += numero;
+            
+            if (numero > 0) {positivos++;}
+            else {negativos++;}
+            
+            i++;
+        } while (i<=10);
+        System.out.println("La suma es: " + suma);
+        System.out.println("El número de positivos es: " + positivos);
+        System.out.println("El número de negativos es: " + negativos);
+        */
+        // Preg 25 Examen
+        double num;
+        //do {
+        System.out.println("Proporciona un número entre 0 y 10: ");
+        num = Double.parseDouble(consola.nextLine());
+        if (num >=9 && num <=10) {System.out.println("A");}
+        else if(num >=8 && num < 9){System.out.println("B");}
+        else if(num >=7 && num < 8){System.out.println("C");}
+        else if(num >=6 && num < 7){System.out.println("D");}
+        else if(num >=0 && num < 6){System.out.println("F");}
+        else {System.out.println("Valor desconocido");}
+        //} while (num != -1);
     }
     
 }
